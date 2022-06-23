e2a8a9ca634cfe75b844c9d128065930cb16a662
Abdullah <ssmmhazz@gmail.com>
2022-06-23 16:06:01 +0500

    '@If'

--- index.js
+++ index.js
@@ -7,22 +7,40 @@ const app = express();
 const { Server } = require("socket.io")
 // app.listen(process.env.PORT ||4001 );
 
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://testdb:testdb12345@testdb.nuqjg.mongodb.net/?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-//   console.log('connected')
-// });
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://testdb:testdb12345@testdb.nuqjg.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+  console.log('connected')
+});
 
 
 
 
+const { Schema } = mongoose;
 
+const textSchema = new Schema({
+  text:  String, 
+  timestamp: Number
+});
+textSchema.path('_id')
 
+const Model = mongoose.model('testdb', textSchema);
 
+setTimeout(()=>{
+  const doc = new Model({
+    text:'string123456',
+    timestamp: 65874974976
+  
+  });
+  doc.save((err, data)=>{
+  console.log(data);
+  console.error(err)
+  })
+}, 3000)
 
 
 
@@ -60,9 +78,12 @@ app.set('port', process.env.PORT || 4000);
 const server = app.listen(app.get('port'), () => { console.log('Express ' + server.address().port) })
 io.attach(server)
 
+var list = []
+
 io.on('connection',(socket)=>{
   console.log('connected')
   socket.on('send', (a)=>{
+list.push(a)
 console.log(a)
 socket.emit('receive', a)
   })
