2d484e0571e73e9a35efdd2009aa78dbb9c02ccd
Abdullah <ssmmhazz@gmail.com>
2022-07-10 11:43:48 +0500

    'join'

--- index.js
+++ index.js
@@ -99,8 +99,8 @@ const io = new Server();
 
 
 
-app.set('port', process.env.PORT || 4000);
-const server = app.listen(app.get('port'), () => { console.log('Express ' + server.address().port) })
+app.set('port', process.env.PORT || 'https://check-app-d.herokuapp.com');
+const server = app.listen(app.get('port'), () => { console.log(process.env.PORT + server.address().port) })
 io.attach(server)
 
 var list = []
